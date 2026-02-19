35f3821f15cb9438b02cd52bf6be4c90c4431857
Oluseyi Mayotomiwa Immanuel <simpayekini@simpa-yekini.local>
2026-02-19 03:11:36 +0100

    chore: deployment test

--- migrate.js
+++ migrate.js
@@ -2,6 +2,15 @@ const { AppDataSource } = require('./dist/config/data-source.config');
 
 AppDataSource.initialize()
     .then(async (ds) => {
+        const dbUrl = process.env.DATABASE_URL;
+        console.log('DEBUG: DATABASE_URL exists:', !!dbUrl);
+        if (dbUrl) {
+            // Mask password for security
+            const maskedUrl = dbUrl.replace(/:([^:@]+)@/, ':***@');
+            console.log('DEBUG: DATABASE_URL:', maskedUrl);
+        } else {
+            console.log('DEBUG: DATABASE_URL is not set!');
+        }
         console.log('Running migrations...');
         await ds.runMigrations();
         console.log('Migrations complete.');
