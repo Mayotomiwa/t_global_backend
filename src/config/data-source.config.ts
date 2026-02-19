9c1ade6a75a3f0b391a685816ef51d1d8da60ac4
Oluseyi Mayotomiwa Immanuel <simpayekini@simpa-yekini.local>
2026-02-19 15:44:54 +0100

    chore: adding missing profile entity

--- src/config/data-source.config.ts
+++ src/config/data-source.config.ts
@@ -3,13 +3,14 @@ import * as dotenv from 'dotenv';
 import { PublicationEntity } from '../features/publications/entities/publication.entity';
 import { ShiftGroupEntity } from '../features/shift/entities/shift-group.entity';
 import { ShiftRoomEntity } from '../features/shift/entities/shift.entity';
+import { ProfileEntity } from '../features/profile/entities/profile.entity';
 
 dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
     url: process.env.DATABASE_URL,
-    entities: [ShiftGroupEntity, ShiftRoomEntity, PublicationEntity],
+    entities: [ShiftGroupEntity, ShiftRoomEntity, PublicationEntity, ProfileEntity],
     migrations: ['dist/migrations/*.js'],
     ssl:
         process.env.NODE_ENV === 'production'
