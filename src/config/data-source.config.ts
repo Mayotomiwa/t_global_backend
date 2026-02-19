0cdbd8adbc470c533d7e24e1f7397999f4a4d47f
Oluseyi Mayotomiwa Immanuel <simpayekini@simpa-yekini.local>
2026-02-19 16:11:29 +0100

    chore:removing profile entity

--- src/config/data-source.config.ts
+++ src/config/data-source.config.ts
@@ -3,14 +3,13 @@ import * as dotenv from 'dotenv';
 import { PublicationEntity } from '../features/publications/entities/publication.entity';
 import { ShiftGroupEntity } from '../features/shift/entities/shift-group.entity';
 import { ShiftRoomEntity } from '../features/shift/entities/shift.entity';
-import { ProfileEntity } from '../features/profile/entities/profile.entity';
 
 dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
     url: process.env.DATABASE_URL,
-    entities: [ShiftGroupEntity, ShiftRoomEntity, PublicationEntity, ProfileEntity],
+    entities: [ShiftGroupEntity, ShiftRoomEntity, PublicationEntity],
     migrations: ['dist/migrations/*.js'],
     ssl:
         process.env.NODE_ENV === 'production'
